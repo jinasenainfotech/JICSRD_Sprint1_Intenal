697f551a545f460891b79176c4cfe1dcdae90592
shanilaSjayakody <32037349+shanilaSjayakody@users.noreply.github.com>
2019-11-07 16:19:27 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = '';
-$config['base_url']    = '';
+ $config['base_url'] = 'https://riskd1.azurewebsites.net';
+//$config['base_url']    = '';
 
 
 /*
